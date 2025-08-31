6bc45e5317b4205eccd1b496e5ab763e64267160
chetandasgondaliya-lgtm <chetandasgondaliya@gmail.com>
2025-08-31 11:59:49 +0530

    Add files via upload

---
+++ m2 task1.py
@@ -0,0 +1,18 @@
+
+
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f" is Even")
+else:
+    print(f" is Odd")
+
+# TASK 2
+
+total = 0
+
+for i in range(1, 51):
+    total += i
+
+print("The sum of integers from 1 to 50 is:", total)
